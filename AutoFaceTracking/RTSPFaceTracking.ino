eea9e717768c90565812727ac15540918d936e27
ideashatch <127272102+ideashatch@users.noreply.github.com>
2023-10-27 09:55:22 +0800

    Add files via upload

---
+++ AutoFaceTracking/RTSPFaceTracking.ino
@@ -0,0 +1,356 @@
+/*
+
+ Example guide:
+ https://www.amebaiot.com/en/amebapro2-amb82-mini-arduino-neuralnework-face-recognition/
+
+ For recommended setting to achieve better video quality, please refer to our Ameba FAQ: https://forum.amebaiot.com/t/ameba-faq/1220
+
+ Face registration commands
+ --------------------------
+ Point the camera at a target face and enter the following commands into the serial monitor,
+ Register face:           "REG={Name}"            Ensure that there is only one face detected in frame
+ Exit registration mode:  "EXIT"                  Stop trying to register a face before it is successfully registered
+ Reset registered faces:  "RESET"                 Forget all previously registered faces
+ Backup registered faces to flash:    "BACKUP"    Save registered faces to flash
+ Restore registered faces from flash: "RESTORE"   Load registered faces from flash
+
+ NN Model Selection
+ -------------------
+ Select Neural Network(NN) task and models using modelSelect(nntask, objdetmodel, facedetmodel, facerecogmodel).
+ Replace with NA_MODEL if they are not necessary for your selected NN Task.
+
+ NN task
+ =======
+ OBJECT_DETECTION/ FACE_DETECTION/ FACE_RECOGNITION
+
+ Models
+ =======
+ YOLOv3 model         DEFAULT_YOLOV3TINY   / CUSTOMIZED_YOLOV3TINY
+ YOLOv4 model         DEFAULT_YOLOV4TINY   / CUSTOMIZED_YOLOV4TINY
+ YOLOv7 model         DEFAULT_YOLOV7TINY   / CUSTOMIZED_YOLOV7TINY
+ SCRFD model          DEFAULT_SCRFD        / CUSTOMIZED_SCRFD
+ MobileFaceNet model  DEFAULT_MOBILEFACENET/ CUSTOMIZED_MOBILEFACENET
+ No model             NA_MODEL
+
+*/
+// Default audio preset configurations:
+// 0 :  8kHz Mono Analog Mic
+// 1 : 16kHz Mono Analog Mic
+// 2 :  8kHz Mono Digital PDM Mic
+// 3 : 16kHz Mono Digital PDM Mic
+
+#include "WiFi.h"
+#include "StreamIO.h"
+#include "VideoStream.h"
+#include "RTSP.h"
+#include "NNFaceDetectionRecognition.h"
+#include "VideoStreamOverlay.h"
+#include "AudioStream.h"
+#include "AudioEncoder.h"
+#include "AmebaServo.h"
+
+#define CHANNEL   0
+#define CHANNELNN 3
+
+// Customised resolution for NN
+#define NNWIDTH 576
+#define NNHEIGHT 320
+
+VideoSetting config(VIDEO_FHD, 30, VIDEO_H264, 0);
+VideoSetting configNN(NNWIDTH, NNHEIGHT, 10, VIDEO_RGB, 0);
+NNFaceDetectionRecognition facerecog;
+RTSP rtsp;
+//StreamIO videoStreamer(1, 1);
+StreamIO videoStreamerFDFR(1, 1);
+StreamIO videoStreamerRGBFD(1, 1);
+
+AudioSetting configA(3);
+Audio audio;
+AAC aac;
+StreamIO audioStreamer(1, 1);   // 1 Input Audio -> 1 Output AAC
+StreamIO avMixStreamer(2, 1);   // 2 Input Video + Audio -> 1 Output RTSP
+
+#define EN_AP_MODE  false
+
+char ssid[] = "WINSTEC_5G";    // Set the SSID
+char pass[] = "2a43b09ff7";        // Set the password
+char apssid[] = "AP_Network_SSID";    // Set the AP SSID
+char appass[] = "12345678";        // Set the AP password
+
+int status = WL_IDLE_STATUS;
+char channel[] = "1"; 
+int ssid_status = 0;   
+bool showRTSPURL = true;
+
+AmebaServo myservo;
+
+IPAddress ip;
+int rtsp_portnum; 
+
+String Tracking_name;
+int current_angle;
+int offset_angle;
+int tran_pixel;
+int count = 0;
+bool LockFocus = false;
+
+// variable to store the servo position
+#define myservo_MAX  180
+int pos = 0;
+
+
+void setup() {
+    Serial.begin(115200);
+ 
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to WPA SSID: ");
+        Serial.println(apssid);
+      if (!EN_AP_MODE)      
+        status = WiFi.begin(ssid, pass);
+      else
+        status = WiFi.apbegin(apssid, appass, channel, ssid_status);
+        
+        // wait 2 seconds for connection:
+        //delay(2000);
+        delay(10000);
+    }
+
+    myservo.attach(8);    //PF7
+    myservo.write(90);
+    current_angle = 90;
+    offset_angle = 0;
+    tran_pixel = 30;
+    ip = WiFi.localIP();
+    
+    // Configure camera video channels with video format information
+    // Adjust the bitrate based on your WiFi network quality
+    config.setBitrate(2 * 1024 * 1024);     // Recommend to use 2Mbps for RTSP streaming to prevent network congestion
+    Camera.configVideoChannel(CHANNEL, config);
+    Camera.configVideoChannel(CHANNELNN, configNN);
+    Camera.videoInit();
+
+    // Configure audio peripheral for audio data output
+    audio.configAudio(configA);
+    audio.begin();
+    // Configure AAC audio encoder
+    aac.configAudio(configA);
+    aac.begin();
+
+
+    // Configure RTSP with corresponding video format information
+    rtsp.configVideo(config);
+    rtsp.configAudio(configA, CODEC_AAC);
+    rtsp.begin();
+    rtsp_portnum = rtsp.getPort();
+
+    // Configure Face Recognition model
+    // Select Neural Network(NN) task and models
+    facerecog.configVideo(configNN);
+    facerecog.modelSelect(FACE_RECOGNITION, NA_MODEL, DEFAULT_SCRFD, DEFAULT_MOBILEFACENET);
+    facerecog.begin();
+    facerecog.setResultCallback(FRPostProcess);
+/*
+    // Configure StreamIO object to stream data from video channel to RTSP
+    videoStreamer.registerInput(Camera.getStream(CHANNEL));
+    videoStreamer.registerOutput(rtsp);
+    if (videoStreamer.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+*/
+    // Configure StreamIO object to stream data from audio channel to AAC encoder
+    audioStreamer.registerInput(audio);
+    audioStreamer.registerOutput(aac);
+    if (audioStreamer.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+
+    // Configure StreamIO object to stream data from video channel and AAC encoder to rtsp output
+    avMixStreamer.registerInput1(Camera.getStream(CHANNEL));
+    avMixStreamer.registerInput2(aac);
+    avMixStreamer.registerOutput(rtsp);
+    if (avMixStreamer.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+    
+    // Start data stream from video channel
+    Camera.channelBegin(CHANNEL);
+
+    // Configure StreamIO object to stream data from RGB video channel to face detection
+    videoStreamerRGBFD.registerInput(Camera.getStream(CHANNELNN));
+    videoStreamerRGBFD.setStackSize();
+    videoStreamerRGBFD.setTaskPriority();
+    videoStreamerRGBFD.registerOutput(facerecog);
+    if (videoStreamerRGBFD.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+    // Start video channel for NN
+    Camera.channelBegin(CHANNELNN);
+
+    // Start OSD drawing on RTSP video channel
+    OSD.configVideo(CHANNEL, config);
+    OSD.begin();
+}
+
+void loop() {
+
+    if (Serial.available() > 0) {
+        String input = Serial.readString();
+        input.trim();
+
+        if (input.startsWith(String("REG="))){
+            String name = input.substring(4);
+            facerecog.registerFace(name);
+        } else if (input.startsWith(String("EXIT"))) {
+            facerecog.exitRegisterMode();
+        } else if (input.startsWith(String("RESET"))) {
+            facerecog.resetRegisteredFace();
+        } else if (input.startsWith(String("BACKUP"))) {
+            facerecog.backupRegisteredFace();
+        } else if (input.startsWith(String("RESTORE"))) {
+            facerecog.restoreRegisteredFace();
+        } else if (input.startsWith(String("ANGLE=")))
+        {
+            String sangle = input.substring(6);
+            int iangle = sangle.toInt();
+            if ((iangle >= 0) && (iangle <= myservo_MAX))
+            {
+              myservo.write(iangle);
+              current_angle = iangle;
+            }
+            else
+            {
+              myservo.write(0);
+              current_angle = 0;
+            }
+        }
+        else if (input.startsWith(String("TRACKING=")))
+        {
+            Tracking_name = input.substring(9);
+        }
+    }
+
+    delay(5000);
+    OSD.createBitmap(CHANNEL);
+    OSD.update(CHANNEL);
+}
+
+// User callback function for post processing of face recognition results
+void FRPostProcess(std::vector<FaceRecognitionResult> results) {
+    uint16_t im_h = config.height();
+    uint16_t im_w = config.width();
+
+  if (showRTSPURL)
+  {
+    Serial.print("Network URL for RTSP Streaming: ");
+    Serial.print("rtsp://");
+    Serial.print(ip);
+    Serial.print(":");
+    Serial.println(rtsp_portnum);
+    Serial.println(" ");
+    showRTSPURL = false;
+  }
+    //printf("Total number of faces detected = %d\r\n", facerecog.getResultCount());
+    OSD.createBitmap(CHANNEL);
+
+    if (facerecog.getResultCount() > 0) {
+        for (uint32_t i = 0; i < facerecog.getResultCount(); i++) {
+            FaceRecognitionResult item = results[i];
+            // Result coordinates are floats ranging from 0.00 to 1.00
+            // Multiply with RTSP resolution to get coordinates in pixels
+            int xmin = (int)(item.xMin() * im_w);
+            int xmax = (int)(item.xMax() * im_w);
+            int ymin = (int)(item.yMin() * im_h);
+            int ymax = (int)(item.yMax() * im_h);
+
+            uint32_t osd_color;
+            if (String(item.name()) == String("unknown")) {
+                osd_color = OSD_COLOR_RED;
+                LockFocus = false;
+
+            } else {
+                osd_color = OSD_COLOR_GREEN;
+                //LockFocus = true;
+            }           
+
+            if (String(item.name()) == Tracking_name)
+            {
+                LockFocus = true;
+                osd_color = OSD_COLOR_WHITE;
+            }
+            else
+            {
+                LockFocus = false;
+            }
+            
+            if (LockFocus)
+            {
+              if (xmax - xmin <= 220)
+              {
+                  tran_pixel = 10;
+              }
+              else if (xmax - xmin <= 285)
+              {
+                  tran_pixel = 20;
+              }
+              else
+              {
+                  tran_pixel = 30;
+              }
+                        
+              count++;
+              if (count > 10)
+              {
+                  if ((xmin < 780) && (current_angle < myservo_MAX ))
+                  {
+                      offset_angle=ceil((780-xmin)/tran_pixel);
+                      
+                      if (current_angle + offset_angle > 180)
+                      {
+                          offset_angle = 0;
+                          current_angle = 180;
+                      }
+                      else
+                      {
+                          current_angle+=offset_angle;  
+                      }
+                      //printf("current_angle l  %d\n\r", current_angle);
+                  }
+                  else if ((xmax > 1140) && (current_angle >= 0))
+                  {
+                      offset_angle= ceil((xmax-1140)/tran_pixel);
+
+                      if (current_angle - offset_angle < 0)
+                      {
+                          offset_angle = 0;
+                          current_angle = 0;
+                      }
+                      else
+                      {
+                          current_angle-=offset_angle;  
+                      }
+                      //printf("current_angle r %d %d\n\r",offset_angle, current_angle);
+                  }
+                  else
+                  {
+                      offset_angle = 0;
+                      //printf("middle or else %d\n\r", current_angle);
+                  }
+                  
+                  myservo.write(current_angle);
+                  count = 0;
+              }
+            }
+            
+            // Draw boundary box
+            OSD.drawRect(CHANNEL, xmin, ymin, xmax, ymax, 3, osd_color);
+            float fdistance = 18600.00/(float)(xmax-xmin)/100.00;
+            // Print identification text above boundary box
+            char text_str[40];
+            snprintf(text_str, sizeof(text_str), "%s %.1fm", item.name(), fdistance);
+            OSD.drawText(CHANNEL, xmin, ymin - OSD.getTextHeight(CHANNEL), text_str, osd_color);
+
+        }
+    }
+    OSD.update(CHANNEL);
+}
